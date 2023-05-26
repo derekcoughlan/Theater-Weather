6712e2dc148c242a98ae23e2283af0c1fca0fdd7
derekcoughlan <coughlan.derek@gmail.com>
2023-05-26 13:26:10 -0700

    Added new data and fixed temp

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function addTheaterTableRow(){
     .then(data => {
         theaterData = data;
         theaterData.forEach(element => {
-            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${element.city}&units=imperial&appid=3ed4321b20cef4195e1946e377b41033`)
+            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${element.City}&units=imperial&appid=3ed4321b20cef4195e1946e377b41033`)
             .then(response => response.json())
             .then(weather => {  
             let table = document.getElementById('myTable');
@@ -13,10 +13,10 @@ function addTheaterTableRow(){
             let c2 = row.insertCell(1);
             let c3 = row.insertCell(2);
             let c4 = row.insertCell(3);
-            c1.innerText = `${element.theater}`;
-            c2.innerText = `${element.city}, ${element.state}`;
+            c1.innerText = `${element.Theater}`;
+            c2.innerText = `${element.City}, ${element.State}`;
             c3.innerText = `${weather.weather[0].description}`;
-            c4.innerText = `${weather.main.temp}°F`;
+            c4.innerText = `${Math.floor(weather.main.temp)}°F`;
         })
         })
     })
@@ -26,27 +26,3 @@ function addTheaterTableRow(){
 }
 
 addTheaterTableRow();
-
-// function displayTheaters(){
-//     fetch('theaters.json')
-//     .then(response => response.json())
-//     .then(data => {
-//         theaterData = data;
-//         theaterData.forEach(element => {
-//             fetch(`https://api.openweathermap.org/data/2.5/weather?q=${element.city}&units=imperial&appid=3ed4321b20cef4195e1946e377b41033`)
-//             .then(response => response.json())
-//             .then(weather => {  
-//             const node = document.createElement('li')
-//             const textnode = document.createTextNode(`${element.theater} in ${element.city}, ${element.state} - ${weather.weather[0].description} at ${weather.main.temp}°F`)
-//             node.appendChild(textnode)
-//             document.getElementById('theaterInfo').appendChild(node)
-//         })
-//         })
-//     })
-//     .catch(error => {
-//         document.getElementById('theaterInfo').innerHTML = "Error"
-//     })
-    
-// }
-
-// displayTheaters()
