e3f814f64be46eab5775d3033263acb60b128880
derekcoughlan <coughlan.derek@gmail.com>
2023-06-08 21:48:09 -0700

    final fixes

--- main.js
+++ main.js
@@ -19,30 +19,73 @@ function filterWeather(){
     }
 }
 
-function addTheaterTableRow(){
-    fetch('theaters.json')
-    .then(response => response.json())
-    .then(data => {
-        data.forEach(element => {
-            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${element.City}&units=imperial&appid=${myKey}`)
-            .then(response => response.json())
-            .then(weather => {  
-            let table = document.getElementById('myTable');
-            let row = table.insertRow(-1);
-            let c1 = row.insertCell(0);
-            let c2 = row.insertCell(1);
-            let c3 = row.insertCell(2);
-            let c4 = row.insertCell(3);
-            c1.innerText = `${element.Theater}`;
-            c2.innerText = `${element.City}, ${element.State}`;
-            c3.innerText = `${weather.weather[0].description}`;
-            c4.innerText = `${Math.floor(weather.main.temp)}°F`;
-        })
-        })
-    })
-    .catch(error => {
-        document.getElementById('myTable').innerText = "Data Error"
-    })
+function sortByGross(){
+    const table = document.getElementById("myTable")
+    const rows = table.getElementsByTagName("tr");
+    let switchIt = true;
+    let shouldSwitch = true;
+    let i = 1;
+    let x = '';
+    let y = '';
+
+    while(switchIt){
+        switchIt = false;
+        for (i = 1; i < (rows.length - 1); i++){
+            shouldSwitch = false;
+            x = rows[i].getElementsByTagName("td")[4];
+            y = rows[i + 1].getElementsByTagName("td")[4];
+
+          if (Number(x.innerHTML) < Number(y.innerHTML)){
+            shouldSwitch = true;
+            break;
+          }
+        }
+        if (shouldSwitch){
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switchIt = true;
+        }
+    }
 }
 
-addTheaterTableRow();
+function addTheaterData(){
+    fetch('https://api.jsonbin.io/v3/b/64815a178e4aa6225eaaec23')
+      .then(response => response.json())
+      .then(data => {
+        let weatherIds = '';
+        for (let i = 0; i < data.record.length; i++){
+          weatherIds += data.record[i].id;
+          if (i < data.record.length - 1){
+            weatherIds += ','
+            }
+        }
+        fetch(`http://api.openweathermap.org/data/2.5/group?id=${weatherIds}&units=imperial&appid=${myKey}`)
+            .then(response => response.json())
+            .then(element => { console.log(element)
+                let table = document.getElementById('myTable');
+                for (let i = 0; i < data.record.length; i++){
+                    console.log(element)
+                    let row = table.insertRow(-1);
+                    let c1 = row.insertCell(0);
+                    let c2 = row.insertCell(1);
+                    let c3 = row.insertCell(2);
+                    let c4 = row.insertCell(3);
+                    let c5 = row.insertCell(4)
+                    c1.innerText = `${data.record[i].Theater}`;
+                    c2.innerText = `${data.record[i].City}, ${data.record[i].State}`;
+                    c3.innerText = `${element.list[i].weather[0].description}`;
+                    c4.innerText = `${Math.floor(element.list[i].main.temp)}°F`;
+                    c5.innerText = `${Number(data.record[i].currentGross)}`
+                } 
+            })
+            .catch(error => alert('Data Error - Check Theater IDs'))
+        })
+        .catch(error => {
+            document.getElementById('myTable').innerText = "Data Error"
+        })
+}
+
+addTheaterData();
+
+
+
+
